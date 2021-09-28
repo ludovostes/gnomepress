0c3285c894d4ebea1a6d85b0450cba19400c56ed
MrPinoBoy <beaupain.thomas@gmail.com>
2021-09-28 16:32:09 +0200

    added punchline

--- wordpress/wp-content/themes/pil-becode/front-page.php
+++ wordpress/wp-content/themes/pil-becode/front-page.php
@@ -1,11 +1,13 @@
-<?php 
+
+<?php
+    //Template Name: Homepage
     get_header(); 
     $punchLine = get_field('punch_line');
     $video = get_field('video');
     $image = get_field('image');
     $titreVideo = get_field('titre');
 ?>
-<?php if($punchline):?>
-    <h1 class = "topbanner-punchline"><?= $punchLine; ?>
+<?php if($punchLine):?>
+    <h1 class = "topbanner-punchline"><?= $punchLine; ?></h1>
 <?php endif; ?>
 <?php get_footer(); ?>
