5d460c30f78e9ee962f3aabbe564eb8ddd9c4af6
MrPinoBoy <beaupain.thomas@gmail.com>
2021-09-29 14:26:15 +0200

    html elements mostly done, started js

--- wordpress/wp-content/themes/pil-becode/front-page.php
+++ wordpress/wp-content/themes/pil-becode/front-page.php
@@ -3,7 +3,17 @@
     //Template Name: Homepage
     get_header(); 
     $punchLine = get_field('punch_line');
+    
     $video = get_field('video');
+    preg_match('/src="(.+?)"/', $video, $matches);
+    $src = $matches[1];
+    $params = array(
+        'controls' => 0
+    );
+    $new_src = add_query_arg($params, $src);
+    echo $new_src;
+    $video = str_replace($src, $new_src, $video);
+
     $image = get_field('image');
     $imageSrc = $image['sizes']['medium'];
     $titreVideo = get_field('titre');
@@ -13,13 +23,17 @@
 <?php endif; ?>
 
 <?php if($video): ?>
+    <div class="topbanner-videocontainer" style="position:relative;display:flex;justify-content:center;">
         <?= $video ?>
+        <button id="playbutton" class="topbanner-videoplaybutton" style="position:absolute;left:50%;top:50%;transform:translate(-50%,-50%);border-radius:50%;width:100px;height:100px;border:none;background:#FFC33B;">Voir la vidéo</button>
     <?php if($titreVideo): ?>
-        <p class ="topbanner-titrevideo"><?= $titreVideo ?></p>
+        <p class ="topbanner-titrevideo" style="position:absolute;left:50%;top:50%;transform:translate(-50%,50px);color:white;"><?= $titreVideo ?></p>
+    </div>
     <?php endif; ?>
 <?php else: ?>
     <?php if($image) : ?>
         <img src="<?= $imageSrc ?>">
     <?php endif; ?>
 <?php endif; ?>
+<script src="./js/playvideo.js"></script>
 <?php get_footer(); ?>
---
+++ wordpress/wp-content/themes/pil-becode/js/playvideo.js
@@ -0,0 +1,7 @@
+video = document.getElementsByTagName("iframe")[0];
+alert("yo mec")
+document.getElementById("playbutton").addEventListener("click", ()=>{
+    alert("yo mec")
+})
+
+document.getElementById("playbutton").style.color = "red"
