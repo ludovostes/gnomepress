cc57e9983ba55e3b7c67810f8647bcc046b91209
MrPinoBoy <beaupain.thomas@gmail.com>
2021-09-29 11:37:03 +0200

    integration html

--- wordpress/wp-content/themes/pil-becode/front-page.php
+++ wordpress/wp-content/themes/pil-becode/front-page.php
@@ -5,10 +5,21 @@
     $punchLine = get_field('punch_line');
     $video = get_field('video');
     $image = get_field('image');
+    $imageSrc = $image['sizes']['medium'];
     $titreVideo = get_field('titre');
 ?>
 <?php if($punchLine):?>
     <h1 class = "topbanner-punchline"><?= $punchLine; ?></h1>
-    <?= $video ?>
+<?php endif; ?>
+
+<?php if($video): ?>
+        <?= $video ?>
+    <?php if($titreVideo): ?>
+        <p class ="topbanner-titrevideo"><?= $titreVideo ?></p>
+    <?php endif; ?>
+<?php else: ?>
+    <?php if($image) : ?>
+        <img src="<?= $imageSrc ?>">
+    <?php endif; ?>
 <?php endif; ?>
 <?php get_footer(); ?>
