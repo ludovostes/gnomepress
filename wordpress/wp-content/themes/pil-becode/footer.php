a293d0b3df22d89078d6af3d7825e169161ab833
MrPinoBoy <beaupain.thomas@gmail.com>
2021-09-30 16:40:27 +0200

    footer presque fini

--- wordpress/wp-content/themes/pil-becode/footer.php
+++ wordpress/wp-content/themes/pil-becode/footer.php
@@ -6,18 +6,30 @@ $tel=get_field('telephone', 'options');
 $email=get_field('email', 'options');
 ?> 
 <footer>
+    <div class="footer-top">
+        <div class="footer-topleft">
+            <p class="footer-name"><?= $nom ?></p>
+            <div class="footer-bottomleft">
+                <a class="footer-email" href="mailto:<?= $email ?>"><?= $email ?></a>
+                <p class="footer-adress1"><?= $adress1 ?></p>
+                <p class="footer-adress2"><?= $adress2 ?></p>
+            </div>
+        </div>
+        <div class="footer-telephonediv">
+            <a class="footer-telephone" href="tel:+<?php echo trim($tel); ?>">+<?= $tel ?></a>
+        </div>
+    </div>
     <?php wp_nav_menu($args = array(
-        'menu' => 'header',
+        'menu' => 'footer',
         'menu_class' => 'menu-footer'
     ));
     ?>
-    <p class="footer-name"><?= $nom ?></p>
-    <div class="footer-telephonediv">
-        <a class="footer-telephone" href="tel:+<?php echo trim($tel); ?>">+<?= $tel ?></a>
+    <div class="footer-notediv">
+        <p class="footer-note footer-noteleft">Le Pôle Image © 2021</p>
+        <a class="footer-notegreylink footer-noteleft" href="#">Politique de confidentialité</a>
+        <p class="footer-note footer-noteright">Design by <a href="http://savoirfaire.digital/">savoir.faire</a></p>
+        <p class="footer-note footer-noteright">Coded by <a href="http://becode.org/">becode.org</a></p>
     </div>
-    <a class="footer-email" href="mailto:<?= $email ?>"><?= $email ?></a>
-    <p class="footer-adress1"><?= $adress1 ?></p>
-    <p class="footer-adress2"><?= $adress2 ?></p>
 </footer>
 </body>
 </html>
