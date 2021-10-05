806e4b13dd68a4f48b907dbee703d65802b8d16a
MrPinoBoy <beaupain.thomas@gmail.com>
2021-10-05 16:57:39 +0200

    started work on searchbar

--- wordpress/wp-content/themes/pil-becode/archive-entreprises.php
+++ wordpress/wp-content/themes/pil-becode/archive-entreprises.php
@@ -4,8 +4,12 @@ get_header();
 $posts = get_posts( array(
     'posts_per_page' => -1,
     'post_type' => 'entreprise'
-));
-if($posts) : ?>
+));?>
+<div class="entreprises-topdiv">
+    <input class="entreprises-searchbar" type="text" placeholder="Chercher une entreprise...">
+    <button class="entreprises-searchbutton">Q</button>
+</div>
+<?php if($posts) : ?>
 <ul class="entreprises-list">
     <?php foreach ($posts as $post) :
     setup_postdata($post); //ça permet d'utiliser the_title etc
@@ -13,13 +17,17 @@ if($posts) : ?>
     $categories = get_field('categories');
     ?>
     <li class="entreprises-listitem">
-        <div class="entreprises-catégories">
-        <h2 class="entreprises-nom"><?php the_title() ?></h2>
-        <?php foreach($categories as $category): if($category->parent == 0):?>
-            <p class="entreprises-catégorie"><?php echo $category->name?></p>
+        <div class="entreprises-title">
+            <div class="entreprises-titleleft">
+            <h2 class="entreprises-nom"><?php the_title() ?></h2>
+            <div class="entreprises-catégories">
+            <?php foreach($categories as $category): if($category->parent == 0):?>
+                <p class="entreprises-catégorie"><?php echo $category->name?></p>
             <?php endif;
             endforeach;
             ?>
+            </div>
+            </div>
             <img id="accordeonButton" src="http://localhost/wp-content/uploads/2021/10/arrow-down.svg" class="entreprises-accordeonbuttonreset entreprises-accordeonbutton">
         </div>
         <div class="entreprises-info displayNone" id="entreprisesInfo">
