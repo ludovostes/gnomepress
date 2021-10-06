b48e29a81590543b54b6fd3f8332f2a63c558d95
MrPinoBoy <beaupain.thomas@gmail.com>
2021-10-06 16:58:58 +0200

    working on responsive

--- wordpress/wp-content/themes/pil-becode/front-page.php
+++ wordpress/wp-content/themes/pil-becode/front-page.php
@@ -1,3 +1,9 @@
-<?php get_header() ?>
-
+<?php get_header() 
+// Template Name: Homepage
+?>
+<?php $categories = get_field('categories');
+foreach($categories as $category) {
+    echo $category->name;
+}
+?>
 <?php get_footer() ?>
