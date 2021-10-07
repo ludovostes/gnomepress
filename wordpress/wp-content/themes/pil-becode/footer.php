f67a652ee5442ca3fd128aeb235f298b1922c4aa
MrPinoBoy <beaupain.thomas@gmail.com>
2021-10-07 12:30:30 +0200

    footer responsive

--- wordpress/wp-content/themes/pil-becode/footer.php
+++ wordpress/wp-content/themes/pil-becode/footer.php
@@ -7,34 +7,36 @@ $email=get_field('email', 'options');
 ?> 
 <footer>
     <section class="footer">
-    <div class="footer-top">
-        <div class="footer-topleft">
-            <p class="footer-name"><?= $nom ?></p>
-            <div class="footer-bottomleft">
-                <a class="footer-email" href="mailto:<?= $email ?>"><?= $email ?></a>
-                <p class="footer-adress1"><?= $adress1 ?></p>
-                <p class="footer-adress2"><?= $adress2 ?></p>
+        <div class="footer-top">
+            <div class="footer-topleft">
+                <p class="footer-name"><?= $nom ?></p>
+                <div class="footer-bottomleft">
+                    <a class="footer-email" href="mailto:<?= $email ?>"><?= $email ?></a>
+                    <p class="footer-adress1"><?= $adress1 ?></p>
+                    <p class="footer-adress2"><?= $adress2 ?></p>
+                </div>
+            </div>
+            <div class="footer-telephonediv">
+                <a class="footer-telephone" href="tel:+<?php echo trim($tel); ?>">+<?= $tel ?></a>
             </div>
         </div>
-        <div class="footer-telephonediv">
-            <a class="footer-telephone" href="tel:+<?php echo trim($tel); ?>">+<?= $tel ?></a>
+        <div class="footer-right">
+            <?php wp_nav_menu($args = array(
+                'menu' => 'footer',
+                'menu_class' => 'menu-footer'
+            ));
+            ?>
+            <div class="footer-notediv">
+                <div class="footer-noteleft">
+                    <p class="footer-note">Le Pôle Image © 2021</p>
+                    <a class="footer-notegreylink" href="#">Politique de confidentialité</a>
+                </div>
+                <div class="footer-noteright">
+                    <p class="footer-note">Design by <a href="http://savoirfaire.digital/">savoirfaire.digital</a></p>
+                    <p class="footer-note">Coded by <a href="http://becode.org/">becode.org</a></p>
+                </div>
+            </div>
         </div>
-    </div>
-    <?php wp_nav_menu($args = array(
-        'menu' => 'footer',
-        'menu_class' => 'menu-footer'
-    ));
-    ?>
-    <div class="footer-notediv">
-        <div class="footer-noteleft">
-            <p class="footer-note">Le Pôle Image © 2021</p>
-            <a class="footer-notegreylink" href="#">Politique de confidentialité</a>
-        </div>
-        <div class="footer-noteright">
-            <p class="footer-note">Design by <a href="http://savoirfaire.digital/">savoirfaire.digital</a></p>
-            <p class="footer-note">Coded by <a href="http://becode.org/">becode.org</a></p>
-        </div>
-    </div>
     </section>
 </footer>
 </body>
